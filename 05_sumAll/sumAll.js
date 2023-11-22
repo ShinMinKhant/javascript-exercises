a24fe6b13f79480f0e12cfe250a2cdf95baffddc
ShinMinKhant <133580286+ShinMinKhant@users.noreply.github.com>
2023-11-22 22:35:39 +0630

    Final exercise!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secNum) {
+    let output = 0;
+    if (firstNum < secNum && (firstNum > 0 && secNum > 0) && (typeof firstNum === 'number' && typeof secNum === 'number')) {
+        for (i = firstNum; i <= secNum; i++) {
+            output += i;
+        }
+        return output;
+    } else if (firstNum > secNum && (firstNum > 0 && secNum > 0) && (typeof firstNum === 'number' && typeof secNum === 'number')) {
+        for (j = firstNum; j >= secNum; j--) {
+            output += j;
+        }
+        return output;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
